4e5c12aca85dad9482b00f1d56aaf7d525ac4098
Ivan Bowen <bowenivan16@gmail.com>
2023-01-08 12:43:26 +0300

    set xata and storyblok

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
 import '../styles/globals.css'
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+import Feature from "../components/Feature";
+import Grid from "../components/Grid";
+import Page from "../components/Page";
+import Teaser from "../components/Teaser";
+import { SessionProvider } from 'next-auth/react';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const components = {
+  feature: Feature,
+  grid: Grid,
+  teaser: Teaser,
+  page: Page,
+};
+
+storyblokInit({
+  accessToken: "6Tbjsm7a83xT3jAyzuoAwwtt",
+  use: [apiPlugin],
+  components
+});
+
+function MyApp({ Component, pageProps, session }) {
+  return(
+    <SessionProvider session={session}>
+          <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
 export default MyApp
